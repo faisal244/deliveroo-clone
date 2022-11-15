267a2896c263b3673d84f50d53887156281108e0
Faisal Mohammed <m.faisal244@gmail.com>
2022-11-15 11:38:59 +0000

    Designed Banner Info section for RestaurantScreen
    - Pulled in rating, address and description data from Sanity CMS backend and got it to render and styled it with Tailwind CSS on the RestaurantScreen view

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -2,7 +2,15 @@ import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { urlFor } from "../sanity";
-import { ArrowLeftIcon } from "react-native-heroicons/outline";
+// import { ArrowLeftIcon } from "react-native-heroicons/outline";
+import { QuestionMarkCircleIcon } from "react-native-heroicons/outline";
+import {
+	ArrowLeftIcon,
+	ArrowRightIcon,
+	MapPinIcon,
+	StarIcon,
+} from "react-native-heroicons/solid";
+import DishRow from "../components/DishRow";
 
 const RestaurantScreen = () => {
 	const navigation = useNavigation();
@@ -47,6 +55,61 @@ const RestaurantScreen = () => {
 						color="#00CCBB"
 					/>
 				</TouchableOpacity>
+			</View>
+			<View className="bg-white">
+				<View className="px-4 pt-4">
+					<Text className="text-3xl font-bold">{title}</Text>
+					<View className="flex-row space-x-2 my-1">
+						<View className="flex-row items-center space-x-1">
+							<StarIcon
+								color="green"
+								opacity={0.5}
+								size={22}
+							/>
+							<Text className="text-xs text-gray-500">
+								<Text className="text-green-500">{rating}</Text> · {genre}
+							</Text>
+						</View>
+
+						<View className="flex-row items-center space-x-1">
+							<MapPinIcon
+								color="gray"
+								opacity={0.4}
+								size={22}
+							/>
+							<Text className="text-xs text-gray-500">Nearby · {address}</Text>
+						</View>
+					</View>
+
+					<Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+				</View>
+
+				<TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+					<QuestionMarkCircleIcon
+						color="gray"
+						opacity={0.6}
+						size={20}
+					/>
+					<Text className="pl-2 flex-1 text-md font-bold">
+						Have a food allergy?
+					</Text>
+					<ArrowRightIcon color="#00CCBB" />
+				</TouchableOpacity>
+			</View>
+
+			<View className="pb-36">
+				<Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+
+				{dishes.map((dish) => (
+					<DishRow
+						key={dish._id}
+						id={dish._id}
+						name={dish.name}
+						description={dish.short_description}
+						price={dish.price}
+						image={dish.image}
+					/>
+				))}
 			</View>
 		</ScrollView>
 	);
