9f5ab529b5568a874982be12ada22d0d189387d3
Faisal Mohammed <m.faisal244@gmail.com>
2022-11-17 11:06:36 +0000

    Completed Basket Screen and restaurantSlice

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { urlFor } from "../sanity";
 // import { ArrowLeftIcon } from "react-native-heroicons/outline";
@@ -12,10 +12,12 @@ import {
 } from "react-native-heroicons/solid";
 import DishRow from "../components/DishRow";
 import BasketIcon from "../components/BasketIcon";
+import { useDispatch } from "react-redux";
+import { setRestaurant } from "../features/restaurantSlice";
 
 const RestaurantScreen = () => {
 	const navigation = useNavigation();
-	// const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
 	const {
 		params: {
@@ -31,6 +33,23 @@ const RestaurantScreen = () => {
 			lat,
 		},
 	} = useRoute();
+
+	useEffect(() => {
+		dispatch(
+			setRestaurant({
+				id,
+				imgUrl,
+				title,
+				rating,
+				genre,
+				address,
+				short_description,
+				dishes,
+				long,
+				lat,
+			})
+		);
+	}, []);
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
